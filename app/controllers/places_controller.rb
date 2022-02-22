662b7f4b89f9c65baaae60691ab6ddc9de63a0cd
mikewise5 <mike.wise@kellogg.northwestern.edu>
2022-02-22 00:39:23 +0000

    Commit 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,21 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+      end
+    
+      def show
+        @place = Place.find(params["name"])
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Place.new(params["name"])
+        @place.save
+        redirect_to "/places"
+      end
+
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+    
+      def new
+        @post = Post.new
+        @place = Place.find(params["place_id"])
+        @post.place_id = @place.id
+      end
+    
+      def create
+        @post = Post.new(params["title"])
+        @post.save
+        redirect_to "/places/#{@post.place.id}"
+      end
+
+
+end
